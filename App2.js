bcc5c1b443cefa996582f71bdbcaf94d1bf852b6
JayamaniArigela4 <123542778+JayamaniArigela4@users.noreply.github.com>
2023-01-30 15:56:40 +0530

    Add files via upload

---
+++ App2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import App1 from"./App1.js";
+function App2() {
+    return(
+        <div>
+            <h1 style={{color:"orange"}}>Take car</h1>
+            <App1></App1>
+        </div>
+    )
+}
+export default App2;
